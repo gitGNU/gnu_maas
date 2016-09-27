13d7123587d41355adcdc7d4592ef11b2e10903f
Aurélien DESBRIÈRES <aurelien.desbrieres@gmail.com>
2016-09-27 23:06:49 +0200

    improvement on the code

--- search.sh
+++ search.sh
@@ -40,9 +40,12 @@ if command_exists apt ; then
     sudo apt search soft_search ; exit
 fi
 
-# For RedHat / Fedora / Centos and derivatives
+# For Fedora and derivatives
 if command_exists dnf ; then
-    sudo dnf search soft_search ; exit
-elif
-    sudo yum search soft_search ; exit
+    sudo dnf install -y soft_exit ; exit
+fi
+
+# For RedHat / CentOS and derivatives
+if command_exists yum ; then
+    sudo yum install -y soft_search ; exit
 fi
